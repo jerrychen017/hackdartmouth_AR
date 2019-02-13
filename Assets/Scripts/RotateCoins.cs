9d87a0f9fc09a13f9e9723baafaeb5046a2f7614
Guangrui Chen <gchen41@jhu.edu>
2019-02-12 23:15:52 -0500

    init commit

---
+++ Assets/Scripts/RotateCoins.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class RotateCoins : MonoBehaviour {
+    // Update is called once per frame
+    void Update () {
+        transform.Rotate(Vector3.right, Time.deltaTime * 70);
+    }
+}
+
---
+++ Assets/Scripts/Walk.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour {
+    public int speed = 5;
+
+    public bool moveforward = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButton ("Fire1"))
+        {
+
+            moveforward = !moveforward;
+        }
+
+        if (moveforward)
+        {
+
+    
+
+        transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Cylindertags"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
+
+
+
